befa6a011f5c04e4f6a020e1e26f2654c61cea20
Slavencer <javiercarrascoarango@gmail.com>
2025-03-08 18:29:29 +0100

    basic start of structure

---
+++ webapp/src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+const NavBar = () => {
+  return (
+        <Typography>Nav</Typography>
+  )
+}
+
+export default NavBar
--- webapp/src/windows/IndexPage.js
+++ webapp/src/windows/IndexPage.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import {Typography} from '@mui/material'
 
 const IndexPage = () => {
   return (
     
-    <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 2 }}>
-    Welcome to the 2025 edition of the Software Architecture course
-  </Typography>
+    <Typography component="h1">What is wichat</Typography>
   )
 }
 
