46d41ec53c1bd6a7a04833dfef853022b882bbbb
Slavencer <javiercarrascoarango@gmail.com>
2025-03-14 12:49:32 +0100

    fix: Attempt to fix the cuality gate

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import LogInOrUpPage from "./windows/LogInOrUpPage";
 import IndexPage from './windows/IndexPage';
 import HomePage from './windows/HomePage';
+import LoggedInRoutes from './LoggedInRoutes';
 import {CssBaseline, ThemeProvider, createTheme} from '@mui/material/';
-import {BrowserRouter as Router,Route,Routes, Navigate} from 'react-router-dom';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 
 function App() {
   const theme = createTheme({
@@ -19,13 +20,7 @@ function App() {
       }
     }
   });
-  const loggedInRoutes = ({ children }) => {
-    const token = localStorage.getItem("sessionToken");
-    if (!token) {
-      return <Navigate to="/auth/true" />;//redirect to login page
-    }
-    return children;
-  };
+  
 
   return (
     <ThemeProvider theme={theme}>
@@ -35,7 +30,7 @@ function App() {
           <Route path='/' element={<IndexPage></IndexPage>} />
           <Route path="/auth/:loginRequested" element={<LogInOrUpPage></LogInOrUpPage>}/>
           <Route path="/auth" element={<LogInOrUpPage></LogInOrUpPage>}/>
-          <Route path="/home" element={<loggedInRoutes><HomePage/></loggedInRoutes>}/>
+          <Route path="/home" element={<LoggedInRoutes><HomePage/></LoggedInRoutes>}/>
         </Routes>
       </Router>
     </ThemeProvider>
