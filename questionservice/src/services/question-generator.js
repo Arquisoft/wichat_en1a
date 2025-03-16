3f1f6f81b51bfddb0b70c5746ff51f39d92d9206
uo294039 <uo294039@uniovi.es>
2025-03-16 14:40:19 +0100

    add shuffleArray const

--- questionservice/src/services/question-generator.js
+++ questionservice/src/services/question-generator.js
@@ -79,6 +79,14 @@ const generateQuestionsFromData = (data, type) => {
     });
 };
 
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];  // Intercambio de elementos
+    }
+    return array;
+};
+
 
 const generateIncorrectOptions = (correctAnswer, data, numOptions) => {
     const incorrectOptions = [];
