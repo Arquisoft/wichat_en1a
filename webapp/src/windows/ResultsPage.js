9e213c0d62b4b40a43cdd69a901bff4482429f94
Slavencer <javiercarrascoarango@gmail.com>
2025-04-02 22:43:23 +0200

    feat: Implement the visual side of the results page

--- webapp/src/windows/ResultsPage.js
+++ webapp/src/windows/ResultsPage.js
@@ -1,17 +1,40 @@
-import { Card, CardContent, Grid, Typography } from '@mui/material'
 import React from 'react'
+import { Avatar, Box, Button, Card, CardContent, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import NavBarSignedIn from '../components/NavBarSignedIn'
 
 const ResultsPage = () => {
+  const stats = [{name:"Questions answered",value:"20"},{name:"Score",value:"400"}];//TODO switch for generated values
   return (
-    <Grid container>
-        <Card>
-            <CardContent>
-                <Typography variant='h2'>Results:</Typography>
-                <Typography variant='body1'>Questions answered: {sessionStorage.getItem('questionNum')}</Typography>
-                <Typography variant='body1'>Questions answered correctly: {sessionStorage.getItem('score')}</Typography>
-            </CardContent>
+    <Box>
+      <NavBarSignedIn />
+      <Box style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
+        <Card sx={{textAlign:"center",bgcolor:"secondary.light"}}>
+          <CardContent>
+            <Avatar sx={{ margin: "auto", width: 60, height: 60 }} />
+            <Typography component="h2" variant="h5">Your statistics:</Typography>
+            <TableContainer sx={{padding:"1rem"}}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell><b>Stat</b></TableCell>
+                    <TableCell><b>Value</b></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {stats.map((stat, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{stat.name}</TableCell>
+                      <TableCell>{stat.value}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <Button fullWidth variant='outlined'>Play again</Button>
+          </CardContent>
         </Card>
-    </Grid>
+      </Box>
+    </Box>
   )
 }
 
