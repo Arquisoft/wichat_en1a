1e9c545f97278667169a5689e94f2b95559f8d43
UO294013 <uo294013@uniovi.es>
2025-04-04 20:52:52 +0200

    Fast push of statistics templates

---
+++ webapp/src/windows/Statistics.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { Card, CardContent, Typography, List, ListItem, ListItemText, Divider } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import NavBar from '../components/NavBarSignedIn';
+import { useTranslation } from 'react-i18next';
+import '../css/Statistics.css';
+
+const Statistics = () => {
+
+};
+
+export default Statistics;
