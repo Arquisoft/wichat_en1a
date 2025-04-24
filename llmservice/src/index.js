907884bb7bf7f1238021e764446b7c66162b516c
MiguelAlvarezHernandez <uo295368@uniovi.es>
2025-04-25 00:56:31 +0200

    fix: port llmservice

--- llmservice/src/index.js
+++ llmservice/src/index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 const port = process.env.PORT || 8003;
 const server = app.listen(port, () => {
-    console.log(`LLM Service listening on port ${port}`);
+    console.log(`LLM Service listening on port http://localhost:${port}`);
 });
 
 module.exports = server;
